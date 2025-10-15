d179eb1546097f8e09103b683485d477633ada2a
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-10-15 11:29:16 +0300

    started teaching Java

---
+++ Lesson 1/4.Type string/main.js
@@ -0,0 +1,14 @@
+// let a = '"hbfbshfsd"';
+// let b = "'hbfbshfsd'";
+// console.log(a, b);
+//
+// let c = `okten`;
+// console.log(c);
+
+let greeting = 'hello';
+let intro = 'my name is vasya';
+
+let msg = greeting + ' ' + intro;
+console.log(msg);
+let msg2 = `${greeting} ${intro}`;
+console.log(msg2);
---
+++ Lesson 1/5.Type number/main.js
@@ -0,0 +1,15 @@
+let a = 100500;  // integer
+console.log(a);
+let b = 3.1232131;  // float - double
+console.log(b);
+
+console.log(10 + 20);
+console.log(10 - 20);
+console.log(10 * 20);
+console.log(10 / 20);
+console.log(11 % 3);
+
+console.log(0.1 + 0.2);
+
+let big =BigInt( 231236127836263637673127213781 );
+console.log(big);
