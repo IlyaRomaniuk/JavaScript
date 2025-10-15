d319b1e654e956aa64534fae970960cddee9d6e2
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-10-15 11:56:09 +0300

    redesigned file

--- Lesson 1/5.Type numbers/main.js
+++ Lesson 1/5.Type numbers/main.js
@@ -11,5 +11,9 @@ console.log(11 % 3);
 
 console.log(0.1 + 0.2);
 
-let big =BigInt( 231236127836263637673127213781 );
+let big =BigInt(231236127836263637673127213781);
 console.log(big);
+
+let n1 = 100;
+let n2 = 200;
+console.log(n1 + n2);
