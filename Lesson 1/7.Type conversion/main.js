1241754af7f45abf2ec2682bd47ebfcab8d6abae
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-10-16 13:11:47 +0300

    finished 7 chapter

--- Lesson 1/7.Type conversion/main.js
+++ Lesson 1/7.Type conversion/main.js
@@ -2,5 +2,17 @@
 // console.log(100 + 200 + "");
 // console.log('' + 100 + 200);
 
-console.log(+'100500asd');
-console.log()
+// console.log(+'100500asd');
+// console.log(parseInt('100500.123'));
+// console.log(parseFloat('100500.123'));
+
+// console.log(!!-1000);
+// console.log(!!0);
+// console.log(!!'asd');
+// console.log(!!''); // false
+//
+// console.log(!!'0'); // true
+
+console.log(true + '');
+console.log(+false);
+console.log(100 - true);
