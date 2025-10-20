662614b050ddcff2819e20b415bbbb496c458b0a
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-10-20 14:01:01 +0300

    finished studying lesson 2

---
+++ Lesson 2/10.Ternary operator/main.js
@@ -0,0 +1,18 @@
+let obj = {
+    id: 1,
+    name: 'vasya',
+    age: 16
+}
+// let access;
+//
+// if (obj.age > 18) {
+//     access = true;
+// } else {
+//     access = false;
+// }
+//
+// console.log(access);
+let access = obj.age > 18 ? 'Yes' : 'No';
+
+
+console.log(access);
