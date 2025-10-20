06c34daedb8be6fa5f0bd56303478b2d67f49883
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-10-20 15:53:36 +0300

    start working hw-2

---
+++ Homeworks/HW 2/hw 2-3/main.js
@@ -0,0 +1,24 @@
+let book1 = {
+    title: 'A',
+    pageCount: 100,
+    genre: 'AA',
+    authors:[
+        {name: 'vasya', age: 19}
+    ]
+}
+let book2 = {
+    title: 'B',
+    pageCount: 200,
+    genre: 'BB',
+    authors:[
+        {name: 'olya', age: 18}
+    ]
+}
+let book3 = {
+    title: 'C',
+    pageCount: 300,
+    genre: 'CC',
+    authors:[
+        {name: 'kolya', age: 10}
+    ]
+}
---
+++ Homeworks/HW 2/hw 2-4/main.js
@@ -0,0 +1,23 @@
+let users = [
+    {name: 'dadad1', username: 'dasda', password: 121},
+    {name: 'dadad2', username: 'dadad', password: 122},
+    {name: 'daada3', username: 'vcxvxv', password: 123},
+    {name: 'dadadw4', username: 'dadxas', password: 124},
+    {name: 'rgfsdfs5', username: 'gregsd', password: 126},
+    {name: 'rwcerw6', username: 'fewfw', password: 125},
+    {name: 'gcewr7', username: 'retretgrf', password: 127},
+    {name: 'ewqee8', username: 'terter', password: 231},
+    {name: 'rewf9', username: 'hghfghf', password: 214},
+    {name: 'rewrdr10', username: 'treterte', password: 1234},
+]
+console.log(users);
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
