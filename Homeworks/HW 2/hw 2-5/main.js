59830e4f3dc8f650509ba8d6939095a09b9c96de
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-10-20 17:28:15 +0300

    finished working hw 2-5

--- Homeworks/HW 2/hw 2-5/main.js
+++ Homeworks/HW 2/hw 2-5/main.js
@@ -1,9 +1,9 @@
 let temps= [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
+    {day: 'Monday', morningTemp: 12, dayTemp: 15, nightTemp: 1},
+    {day: 'Tuesday', morningTemp: 12, dayTemp: 12, nightTemp: 0},
+    {day: 'Wednesday', morningTemp: 12, dayTemp: 17, nightTemp: 1},
+    {day: 'Thursday', morningTemp: 10, dayTemp: 15, nightTemp: 2},
+    {day: 'Friday', morningTemp: 11, dayTemp: 19, nightTemp: 3},
+    {day: 'Saturday', morningTemp: 16, dayTemp: 20, nightTemp: 4},
+    {day: 'Sunday', morningTemp: 14, dayTemp: 18, nightTemp: 5}
 ]
