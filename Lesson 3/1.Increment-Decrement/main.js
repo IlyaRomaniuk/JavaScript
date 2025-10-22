1f3bd51d8b38fb1a745a191a1973f19851da58a2
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-10-22 16:50:06 +0300

    finished studying Lesson 3

---
+++ Lesson 3/1.Increment-Decrement/main.js
@@ -0,0 +1,22 @@
+// let x = 0;
+// x = x + 1; // = 1
+// console.log(x);
+// x++;
+// console.log(x); // = 2
+// x += 1;
+// console.log(x); // = 3
+//
+// x = x - 1; // 2
+// console.log(x);
+// x -= 1; // 1
+// console.log(x);
+// x--;
+// console.log(x); // 0
+
+// let x = 0;
+// ++x;
+// console.log(x);
+
+let a = 0;
+let b = ++a;
+console.log(a, b); // a = 1 b = 1
---
+++ Lesson 3/3.The for cycle/main.js
@@ -0,0 +1,25 @@
+let users = [
+    {name: 'vasya', age: 29, status: true},
+    {name: 'olya', age: 23, status: false},
+    {name: 'katya', age: 43, status: true},
+    {name: 'sergey', age: 54, status: true},
+    {name: 'anton', age: 32, status: false},
+    {name: 'ilya', age: 43, status: false},
+    {name: 'petya', age: 12, status: false},
+    {name: 'kolya', age: 32, status: true},
+    {name: 'anna', age: 44, status: true},
+    {name: 'ira', age: 55, status: true},
+    {name: 'vlad', age: 66, status: true},
+    {name: 'vlad', age: 66, status: true},
+    {name: 'vlad', age: 66, status: true},
+    {name: 'vlad', age: 66, status: true},
+    {name: 'vlad', age: 66, status: true},
+    {name: 'vlad', age: 66, status: true},
+    {name: 'vlad', age: 66, status: true},
+    {name: 'vlad', age: 66, status: true}
+];
+
+for (let i = 0; i < users.length; i++) {
+    let user = users[i];
+    document.write(`<h3>${user.name} ${user.age} - ${user.status}</h3>`);
+}
