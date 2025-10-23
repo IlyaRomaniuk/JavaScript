150b9f40b90d5801c87718188d5ce666a376e627
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-10-23 16:19:26 +0300

    finished executing HW-3-2

---
+++ Homeworks/HW 3/Additional hw/hw 3-3/main.js
@@ -0,0 +1,44 @@
+let numbers = [2,17,13,6,22,31,45,66,100,-18];
+
+// let i = 0
+// while (i < numbers.length) {
+//     let number = numbers[i];
+//     console.log(number);
+//     i++;
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+//         console.log(numbers)
+// }
+
+// let i = 0
+// while (i < numbers.length) {
+//     if (i % 2 !== 0) {
+//         console.log(numbers[i]);
+//     }
+//     i++;
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(numbers[i])
+//     }
+// }
+
+// let i = 0
+// while (i < numbers.length) {
+//     if (i % 2 === 0) {
+//         console.log(numbers[i])
+//     }
+//     i++;
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (i % 2 === 0) {
+//         console.log(numbers[i])
+//     }
+// }
+
+
+
+
