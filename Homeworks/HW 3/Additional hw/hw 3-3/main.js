1c8def7288bab0023c8414fd7ae691b60bbaa36d
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-10-24 09:47:27 +0300

    redesigned hw 3-3

--- Homeworks/HW 3/Additional hw/hw 3-3/main.js
+++ Homeworks/HW 3/Additional hw/hw 3-3/main.js
@@ -39,6 +39,67 @@ let numbers = [2,17,13,6,22,31,45,66,100,-18];
 //     }
 // }
 
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 3 === 0) {
+//         numbers[i] = 'octen';
+//     }
+// }
+// console.log(numbers);
+
+// numbers.reverse();
+// console.log(numbers);
+
+// let i = numbers.length -1;
+// while (i >= 0) {
+//     console.log(numbers[i]);
+//     i--;
+// }
+
+// for (let i = numbers.length -1; i >= 0; i--) {
+//     console.log(numbers[i]);
+// }
+
+// let i = numbers.length -1;
+// while (i >= 0) {
+//     if (numbers[i] % 2 !== 0) {
+//         console.log(numbers[i]);
+//     }
+//     i--;
+// }
+//
+// for (let i = numbers.length - 1; i >= 0; i--) {
+//     if (i % 2 !== 0) {
+//         console.log(numbers[i]);
+//     }
+// }
+
+// let i = numbers.length -1;
+// while (i >= 0) {
+//     if (i % 2 === 0) {
+//         console.log(numbers[i])
+//     }
+//     i--;
+// }
+
+// for (let i = numbers.length - 10; i >= 0; i--) {
+//     if (i % 2 === 0) {
+//         console.log(numbers[i]);
+//     }
+// }
+
+// for (let i = numbers.length; i >= 0; i--) {
+//     if (numbers[i] % 3 === 0) {
+//         numbers[i] = 'octen';
+//     }
+// }
+// console.log(numbers);
+
+
+
+
+
+
+
 
 
 
