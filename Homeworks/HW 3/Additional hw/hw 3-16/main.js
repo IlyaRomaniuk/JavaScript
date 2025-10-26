5bdcba74ea615673b913c18af6f873693d15f923
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-10-26 18:31:24 +0200

    redesigned hw 3-16

--- Homeworks/HW 3/Additional hw/hw 3-16/main.js
+++ Homeworks/HW 3/Additional hw/hw 3-16/main.js
@@ -1,7 +1,7 @@
 let books = [
-    {name: 'The Very Hungry Caterpillar', pages: 50, authors: 1, genre: 'adventures'},
-    {name: 'The Very Hungry Caterpillar', pages: 50, authors: 1, genre: 'adventures'},
-    {name: 'The Very Hungry Caterpillar', pages: 50, authors: 1, genre: 'adventures'},
-    {name: 'The Very Hungry Caterpillar', pages: 50, authors: 1, genre: 'adventures'},
-    {name: 'The Very Hungry Caterpillar', pages: 50, authors: 1, genre: 'adventures'},
+    {name: 'The Very Hungry Caterpillar', pages: 300, authors: 'Eric Carle', genre: ['adventures', 'mystery'] },
+    {name: 'Harry Potter', pages: 500, authors: 'J. K. Rowling', genre: ['adventures', 'fantasy'] },
+    {name: "Where's Spot?", pages: 301, authors: 'Eric Hill', genre: ['adventures'] },
+    {name: "Goodnight Moon", pages: 200, authors: 'Margaret Wise Brown', genre: ['adventures']},
+    {name: "Curious George", pages: 120, authors: 2, genre: 'adventures'},
 ]
