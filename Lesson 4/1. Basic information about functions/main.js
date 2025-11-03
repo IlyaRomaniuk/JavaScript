5a54a1322e6b287f537067981b272052cf8ebe35
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-11-03 17:12:06 +0300

    finished work less  4-1

---
+++ Lesson 4/1. Basic information about functions/main.js
@@ -0,0 +1,19 @@
+// function writer(productTitle, price, descr) {
+//     document.write(
+//         `
+//     <div>
+//         <h2>${productTitle} ${price}</h2>
+//         <p>${descr}</p>
+//     </div>
+//     `
+//     );
+// }
+// writer('milk', 32, 'some milk');
+//
+// writer('meat', 33, 'dadadadada');
+
+function printerArray(array) {
+    for (let item of array) {
+        console.log(item);
+    }
+}
