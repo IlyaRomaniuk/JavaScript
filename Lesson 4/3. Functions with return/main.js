4ad1173136defe57642e87659e2253cc5596cf7f
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-11-05 17:32:39 +0300

    stopped at less 4-3

---
+++ Lesson 4/3. Functions with return/main.js
@@ -0,0 +1,23 @@
+// // let r1 = 10 + 20;
+// // let r2 = 20 + 22;
+//
+// function calc(a, b) {
+//     let result = a + b;
+//     return result;
+// }
+//
+// let r1 = calc(10, 20);
+// let r2 = calc(20, 22);
+// console.log(r1, r2);
+
+// 1000000 pdv warTax
+
+// function percentage(sum, per) {
+//     return sum / 100 * per
+// }
+//
+// function tax(sum) {
+//     return sum - percentage(sum, 20) - percentage(sum, 1.5);
+// }
+// let number = tax(10000000);
+// console.log(number)
