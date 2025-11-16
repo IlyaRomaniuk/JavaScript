0d260c7160fa0e4e8d8c1ae40bf90c89c98af0c3
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-11-16 16:00:38 +0300

    repeated the lesson 4-4

--- Lesson 4/4. The arguments variable in functions/main.js
+++ Lesson 4/4. The arguments variable in functions/main.js
@@ -6,12 +6,12 @@
 //     return a + b + c;
 // }
 
- function foobar() {
-     console.log(arguments);
-     if (arguments.length === 2) {
-         return arguments[0] + arguments[1];
-     }else if (arguments.length === 3) {
-         return arguments[1] + arguments[2] + arguments[3];
-     }
- }
- foobar(1, 2 , 3);
+function foobar() {
+    console.log(arguments);
+    if (arguments.length === 2) {
+        return arguments[0] + arguments[1];
+    }else if (arguments.length === 3) {
+        return arguments[0] + arguments[1] + arguments[2];
+    }
+}
+console.log(foobar(10, 20));
