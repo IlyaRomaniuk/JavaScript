833b7494718ee36a2fbff447dfbe2b7a9fa00ceb
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-11-17 16:35:56 +0300

    stopped at less 6-2

---
+++ Lesson 6/1. String functions/main.js
@@ -0,0 +1,15 @@
+let str = 'hello okten';
+// let s = str.concat('!!!')
+// console.log(s);
+
+console.log(str.toUpperCase());
+console.log(str.startsWith('hel'));
+console.log(str.endsWith('en'));
+console.log(str.substring(0, 7));
+console.log(str.indexOf('o'));
+console.log(str.lastIndexOf('o'));
+console.log(str.charAt(8));
+console.log(str.replace('e', '!'));
+console.log(str.replaceAll('e', '!'));
+let split = str.split(' ');
+console.log(split);
