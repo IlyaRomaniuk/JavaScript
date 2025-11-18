7c6c003525e54dbfe9ff5dafed542cd79b14c7ab
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-11-18 16:55:44 +0300

    stopped at hw 6-5

---
+++ Homeworks/HW 6/hw 6-2/main.js
@@ -0,0 +1,17 @@
+let s = 'hello world';
+const s1 = s.toUpperCase();
+console.log(s1);
+
+let st = 'lorem ipsum';
+const s2 = st.toUpperCase();
+console.log(s2);
+
+let str = 'javascript is cool';
+const s3 = str.toUpperCase();
+console.log(s3);
+
+
+
+
+
+
---
+++ Homeworks/HW 6/hw 6-3/main.js
@@ -0,0 +1,11 @@
+let s = 'HELLO WORLD';
+const s1 = s.toLowerCase();
+console.log(s1);
+
+let st = 'LOREM IPSUM';
+const s2 = st.toLowerCase();
+console.log(s2);
+
+let str = 'JAVASCRIPT IS COOL';
+const s3 = str.toLowerCase();
+console.log(s3);
