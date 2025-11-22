ae5f9c1605373078fa23e5a074a18a593776a7d1
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-11-22 14:01:35 +0300

    redesigned at hw 7-6

--- Homeworks/HW 7/hw 7-6/main.js
+++ Homeworks/HW 7/hw 7-6/main.js
@@ -6,9 +6,18 @@ function Car (model, manufacturer, yearOfManufacture, maximumSpeed, engineCapaci
     this.engineCapacity = engineCapacity;
     this.drive = function () {
         console.log(`їдемо зі швидкістю ${this.maximumSpeed}`);
+    };
+    this.info = function () {
+        for (const key in this) {
+            console.log(key, this[key]);
+        }    
+    };
+    this.increaseMaxSpeed = function (newSpeed) {
+        this.maximumSpeed = this.maximumSpeed + newSpeed;
     }
-}
+    this.changeYear = function (newYear) {
+        this.yearOfManufacture = newYear;
+    }
+};
 let toyotaCar = new Car ('toyota', 'japan',2025, '180 km/hours', '2.0L')
 console.log(toyotaCar);
-
-toyotaCar.drive();
