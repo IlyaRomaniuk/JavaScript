3e1d676808fac5aa36ee22a6ceb3acc1c5f4c7b8
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-11-26 16:59:23 +0300

    stopped at the less 9-1

---
+++ Lessons/Lesson 9/1. DOM Document Object Model and element search/main.js
@@ -0,0 +1,10 @@
+// console.log(document.head.children);
+// console.log(document.body);
+// console.log(document.head.innerHTML);
+// console.log(document.body.innerHTML);
+
+// let ul1 = document.getElementById('list-1');
+// console.log(ul1);
+
+let menuCollection = document.getElementsByClassName('menu');
+console.log(menuCollection);
