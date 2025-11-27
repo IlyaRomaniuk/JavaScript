cb39907b0ff02a785f83e37cdf695c4fa97a8b11
IlyaRomaniuk <yourcasualhome@gmail.com>
2025-11-27 11:12:29 +0300

    redesigned at hw 9-1

--- Lessons/Lesson 9/1. DOM Document Object Model and element search/main.js
+++ Lessons/Lesson 9/1. DOM Document Object Model and element search/main.js
@@ -6,5 +6,26 @@
 // let ul1 = document.getElementById('list-1');
 // console.log(ul1);
 
-let menuCollection = document.getElementsByClassName('menu');
-console.log(menuCollection);
+// let menuCollection = document.getElementsByClassName('menu');
+// console.log(menuCollection);
+// for (const menuCollectionElement of menuCollection) {
+//     console.log(menuCollectionElement);
+// }
+
+// let liList = document.getElementsByTagName('li');
+// console.log(liList);
+
+// let list1 = document.getElementById('list-1');
+// console.log(list1);
+
+// let list1LiCollection = document.getElementsByTagName('li');
+// console.log(list1LiCollection);
+
+// console.log(document.querySelector('.menu'));
+// console.log(document.querySelectorAll('.menu'));
+
+let nodeListOf = document.querySelectorAll('.menu:nth-child(2)>li');
+console.log(nodeListOf);
+for (const node of nodeListOf) {
+    console.log(node);
+}
